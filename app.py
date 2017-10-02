a08246e0cf97356bb66602cab65f53ea5c1a857c
Monis Rizvi <monisriz@gmail.com>
2017-10-02 17:36:30 -0500

    HTML form/AWS-SES deployment

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import os
-
+import boto3
 import tornado.ioloop
 import tornado.web
 import tornado.log
 
+from dotenv import load_dotenv
 from jinja2 import \
   Environment, PackageLoader, select_autoescape
+
+load_dotenv(".env")
 
 PORT = int(os.environ.get('PORT', '8888'))
 
@@ -14,6 +17,13 @@ print(GIT_REV)
 ENV = Environment(
   loader=PackageLoader('app', 'templates'),
   autoescape=select_autoescape(['html', 'xml'])
+)
+
+SES_CLIENT = boto3.client(
+    'ses',
+    aws_access_key_id=os.environ.get('AWS_ACCESS_KEY'),
+    aws_secret_access_key=os.environ.get('AWS_SECRET_KEY'),
+    region_name="us-east-1"
 )
 
 class TemplateHandler(tornado.web.RequestHandler):
@@ -36,9 +46,55 @@ class CityHandler(TemplateHandler):
           'no-store, no-cache, must-revalidate, max-age=0')
         self.render_template(city + ".html", {'city': city})
 
+class FormHandler(TemplateHandler):
+  def post (self):
+    firstname = self.get_body_argument('firstname')
+    lastname = self.get_body_argument('lastname')
+    email = self.get_body_argument('email')
+    message = self.get_body_argument('message')
+
+    response = SES_CLIENT.send_email(
+      Destination={
+        'ToAddresses': ['monisriz@gmail.com'],
+      },
+      Message={
+        'Body': {
+          'Text': {
+            'Charset': 'UTF-8',
+            'Data': 'First Name: {}\nLast Name: {}\nEmail: {}\nMessage: {}\n'.format(firstname, lastname, email, message),
+          },
+        },
+        'Subject': {'Charset': 'UTF-8', 'Data': 'Travel Page Section'},
+      },
+      Source='contact@monisrizvi.com',
+    )
+
+    self.redirect('submit.html?firstname=' + firstname)
+
+  def get(self):
+    self.set_header(
+      'Cache-Control',
+      'no-store, no-cache, must-revalidate, max-age=0')
+    self.render_template("form.html", {"firstname":{}})
+
+
+class SubmitHandler(TemplateHandler):
+  def get(self):
+    name = self.get_query_argument('firstname')
+    self.set_header(
+      'Cache-Control',
+      'no-store, no-cache, must-revalidate, max-age=0')
+    self.render_template("submit.html", {'firstname': name})
+
+  def post (self):
+      name = self.get_body_argument('firstname')
+
+
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
+        (r"/form.html", FormHandler),
+        (r"/submit.html",SubmitHandler),
         (r"/(.*).html", CityHandler),
         (r"/static/(.*)", tornado.web.StaticFileHandler, {'path': 'static'})
     ], autoreload=True)
